df7954338cbf82ff2eb4fa10e88546da1ffe9a8a
shantamshrestha <shantamshrestha@gmail.com>
2024-01-07 21:24:12 +0545

    fixes for mouse over scroll

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ document.addEventListener('DOMContentLoaded', function () {
     trackerElement.style.background = 'radial-gradient(rgba(255, 255, 255, 0.05) 20%, transparent 70%)';
     trackerElement.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
     
-    
     document.body.appendChild(trackerElement);
 
     document.addEventListener('mousemove', (e) => {
-        const xPos = e.clientX;
-        const yPos = e.clientY;
-    
+        const scrollX = window.scrollX || window.pageXOffset;
+        const scrollY = window.scrollY || window.pageYOffset;
+
+        const xPos = e.clientX + scrollX;
+        const yPos = e.clientY + scrollY;
+
         trackerElement.style.left = `${xPos}px`;
         trackerElement.style.top = `${yPos}px`;
-    
+
         // Add light-emerging effect
         trackerElement.style.opacity = 10;
         trackerElement.style.transform = 'scale(10)';
     });
-    
-    
+
     document.addEventListener('mouseover', () => {
         // Show the div when the mouse is over the document
         trackerElement.style.opacity = 10;
         trackerElement.style.transform = 'scale(10)';
     });
 });
-
